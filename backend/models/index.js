f38dddc74a9f8f35278d924a99296a39d4b70057
Josias Besodia <djodev23@gmail.com>
2025-03-24 11:11:10 +0100

    db connection config

---
+++ backend/models/index.js
@@ -0,0 +1,29 @@
+const { sequelize, Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        logging: false,
+        pool: {
+            max: 20,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+);
+
+async function checkDatabaseConnection() {
+    try {
+        await sequelize.authenticate();
+        console.log('✅ Connecté à la base de données MySQL avec Sequelize');
+    } catch (err) {
+        console.error('❌ Erreur de connexion à MySQL :', err);
+        process.exit(1);
+    }
+}
