d1001ead71e28d2a4af3d5c1139b0c5ed5bac882
Josias Besodia <djodev23@gmail.com>
2025-03-24 21:28:55 +0100

    change1

--- backend/controllers/productsController.js
+++ backend/controllers/productsController.js
@@ -3,7 +3,12 @@ const Products = requrie('../models/Products.js');
 exports.addProduct = async (req, res) => {
     try {
         const { category_id,product_name,description,price,stock,image } = req.body;
-        
+        const productExist = await Products.findOne({ where: { product_name, price }});
+        if (productExist) return res.json({ success: false, message: 'Ce produit existe déjà, cliquez sur le button modifier pour le modifier'});
+
+        const newProduct = await Products.create({ category_id,product_name,description,price,stock,image });
+        console.log(newProduct);
+        return res.json({ success: true, message: 'Nouveau produit ajouté avec succès'});
     } catch (err) {
         console.log('Une erreur s\'est produite : ', err);
         return res.json({ success: false, message: 'Une erreur s\'est produite'});
