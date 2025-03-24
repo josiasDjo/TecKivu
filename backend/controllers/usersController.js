3be0444efd0404a39e97a3eda96605185a4fe748
Josias Besodia <djodev23@gmail.com>
2025-03-24 18:02:22 +0100

    Controlleur SET

---
+++ backend/controllers/usersController.js
@@ -0,0 +1,17 @@
+const User = require('../models/Users');
+
+exports.createUser = async (req, res) => {
+
+};
+
+exports.getUser = async (req, res) => {
+
+};
+
+exports.updateUser = async (req, res) => {
+
+};
+
+exports.deleteUser = async (req, res) => {
+    
+}
