c3d9683e23082f7f89a7e631b470eaf26dab4c49
Josias Besodia <djodev23@gmail.com>
2025-03-24 18:12:44 +0100

    change1

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,7 +1,13 @@
 const User = require('../models/Users');
-
+const bcrypt = require('bcrypt');
 exports.createUser = async (req, res) => {
+    const { username, email, password, first_name, last_name, role } = req.body;
+    const EmailExist = await User.findOne({ where: { email}});
+    if (EmailExist) return { success: false, message: 'Cet Email existe déjà '}
 
+    const saltRounds = 10;
+    const password_user = await bcrypt.hash(password, saltRounds);
+    const newUser = await User.create({ username, email, password, first_name, last_name, role });
 };
 
 exports.getUser = async (req, res) => {
@@ -13,5 +19,5 @@ exports.updateUser = async (req, res) => {
 };
 
 exports.deleteUser = async (req, res) => {
-    
+
 }
