314bab816d7dfdc59420742563d338456ccf2bf8
Josias Besodia <djodev23@gmail.com>
2025-03-24 17:35:27 +0100

    change4

--- backend/models/Reviews.js
+++ backend/models/Reviews.js
@@ -1,8 +1,21 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('./index');
+const Product = require('./Products');
+const User = require('./Users');
 
 const Review = sequelize.define('', {
-    
+    review_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    product_id: { type: DataTypes.INTEGER, allowNull: false },
+    user_id: { type: DataTypes.INTEGER, allowNull: false },
+    rating: { type: DataTypes.INTEGER, allowNull: false },
+    comment_user: { type: DataTypes.TEXT, allowNull: true }
 }, {
     timestamp: true
 });
+
+User.HasMany(Review, { foreignKey: 'user_id'});
+Product.HasMany(Review, { foreignKey: 'product_id'});
+Review.BelongsTo(User, { foreignKey: 'user_id' });
+Review.BelongsTo(Product, { foreignKey: 'product_id'});
+
+module.exports = Review;
