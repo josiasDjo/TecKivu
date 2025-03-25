9a4ba2f7197fb8fb41d497f1279c49108cec14e0
Josias Besodia <djodev23@gmail.com>
2025-03-25 10:39:20 +0100

    change19

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,3 +1,6 @@
+// script.js
+import { gsap } from 'gsap';
+
 gsap.from([logo_teckivu, text_navbar_annim, anim_title, anim_subtitle, anim_btn_action], {
     duration: 0.6, delay: 0.2, opacity: 0, stagger: 0.2
 });
