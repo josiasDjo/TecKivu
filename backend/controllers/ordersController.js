7181ad92d4280b68be4da0dcc9b8936740f7e415
Josias Besodia <djodev23@gmail.com>
2025-03-25 06:38:16 +0100

    change1

--- backend/controllers/ordersController.js
+++ backend/controllers/ordersController.js
@@ -1,10 +1,18 @@
-const Orders = require('../models/Orders');
+const Order = require('../models/Orders');
 
 exports.addOrder = async (req, res) => {
     try {
         const { user_id,order_status,total_amount,shipping_address,billing_adsress,payment_method } = req.body;
+        const newOrder = await Order.create({ user_id,order_status,total_amount,shipping_address,billing_adsress,payment_method });
+        return res.json({ success: true, message: 'Commande envoyée'});
     } catch(err) {
         console.log('Une erreur s\'est produite : ', err);
         return res.json({ success: false, message: 'Une erreur s\'est produite'});
+    }
+}
+
+exports.updateOrder = async (req, res) => {
+    try {
+        const 
     }
 }
